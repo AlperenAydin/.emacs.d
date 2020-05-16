be32bbeafc5f00f5bc4ef55a21e70bfdb78a6b7a
Alperen AYDIN <aydin.alperen.bugra@gmail.com>
2020-05-16 12:35:39 +0200

    Added yaml-mode

--- languages/languages.el
+++ languages/languages.el
@@ -2,9 +2,17 @@
 ;; Collect all the language configurations in a single file
 ;; --------------------------------------
 
-;; Language Configs
+;; Major Language Configs
 ;; --------------------------------------
 (require '_python)
+
+;; Minor Language configs
+
+(load-packages
+ '('yaml-mode))
+
+;; YAML
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 
 ;; --------------------------------------
 (provide 'languages)
